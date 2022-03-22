2dc7774a142cad8a591fe0ab14382d3236d7174e
EmStephenson <eStephenson.ext@torchtechnologies.com>
2022-03-22 12:13:46 -0500

    Swapped file names because our client is closer to this server. Variable name swapping next

--- udp_client.c
+++ udp_client.c
@@ -5,15 +5,15 @@
 
 int main(void){
     int socket_desc;
-    struct sockaddr_in server_addr;
+    struct sockaddr_in server_addr, client_addr;
     char server_message[100], client_message[100];
-    int server_struct_length = sizeof(server_addr);
+    int client_struct_length = sizeof(client_addr);
     
     // Clean buffers:
     memset(server_message, '\0', sizeof(server_message));
     memset(client_message, '\0', sizeof(client_message));
     
-    // Create socket:
+    // Create UDP socket:
     socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
     if(socket_desc < 0){
@@ -27,29 +27,40 @@ int main(void){
     server_addr.sin_port = htons(2000);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     
-    // Get input from the user:
-    printf("Enter message: ");
-    gets(client_message);
-    
-    // Send the message to server:
-    if(sendto(socket_desc, client_message, strlen(client_message), 0,
-         (struct sockaddr*)&server_addr, server_struct_length) < 0){
-        printf("Unable to send message\n");
+    // Bind to the set port and IP:
+    if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        printf("Couldn't bind to the port\n");
         return -1;
     }
+    printf("Done with binding\n");
     
-    // Receive the server's response:
-    if(recvfrom(socket_desc, server_message, sizeof(server_message), 0,
-         (struct sockaddr*)&server_addr, &server_struct_length) < 0){
-        printf("Error while receiving server's msg\n");
+    printf("Listening for incoming messages...\n\n");
+    
+    // Receive client's message:
+    if (recvfrom(socket_desc, client_message, sizeof(client_message), 0,
+         (struct sockaddr*)&client_addr, &client_struct_length) < 0){
+        printf("Couldn't receive\n");
         return -1;
     }
+    printf("Received message from IP: %s and port: %i\n",
+           inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
     
-    printf("Server's response: %s\n", server_message);
+    printf("Msg from client: %s\n", client_message);
+    
+    // Change to uppercase:
+    for(int i = 0; client_message[i]; ++i)
+        client_message[i] = toupper(client_message[i]);
+    
+    // Respond to client:
+    strcpy(server_message, client_message);
+    
+    if (sendto(socket_desc, server_message, strlen(server_message), 0,
+         (struct sockaddr*)&client_addr, client_struct_length) < 0){
+        printf("Can't send\n");
+        return -1;
+    }
     
     // Close the socket:
     close(socket_desc);
-    
-    return 0;
-}
 
+}
