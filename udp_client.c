f76fea527e6f96ac63ad34f497d502bd8c7e87c1
EmStephenson <eStephenson.ext@torchtechnologies.com>
2022-03-22 19:29:03 -0500

    added loops, everything works perfectly except client sequence number

--- udp_client.c
+++ udp_client.c
@@ -29,15 +29,11 @@ int main(void){
     int i;
     int varNum = 1;
     
-    // Clean buffers:
-    memset(server_message, '\0', sizeof(server_message));
-    memset(client_message, '\0', sizeof(client_message));
-    
     // Create UDP socket:
     socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
     if(socket_desc < 0){
-        printf("Error while creating socket\n");
+        printf("Error creating socket\n");
         return -1;
     }
     printf("Socket created successfully\n");
@@ -52,48 +48,54 @@ int main(void){
         printf("Couldn't bind to the port\n");
         return -1;
     }
-    printf("Done with binding\n");
+    printf("Bound to port\n");
     
     printf("Listening for incoming messages...\n\n");
     
-    // Receive servers's message:
-    if (recvfrom(socket_desc, server_message, sizeof(server_message), 0,
-         (struct sockaddr*)&server_addr, &server_struct_length) < 0){
-        printf("Couldn't receive\n");
-        return -1;
+    //Loop the main logic 100 times to recieve 100 packets
+    for(int i = 0; i < 100; i++){
+
+        // Clean buffers:
+        memset(server_message, '\0', sizeof(server_message));
+        memset(client_message, '\0', sizeof(client_message));
+
+        // Receive servers's message:
+        if (recvfrom(socket_desc, server_message, sizeof(server_message), 0,
+            (struct sockaddr*)&server_addr, &server_struct_length) < 0){
+            printf("Couldn't receive\n");
+            return -1;
+        }
+    
+        //parse server_message
+        printf("Sequence Number: "); 
+        char *pt;
+        pt = strtok(server_message,",");
+            printf("%s\n", pt);
+            pt = strtok(NULL, ",");
+            printf("Source Address: %s\n", inet_ntoa(server_addr.sin_addr));
+            printf("GPS Position: %s, ", pt);
+            pt = strtok(NULL, ",");
+            printf("%s\n", pt);
+            pt = strtok(NULL, ",");
+            printf("Velocity Magnitude: %s\n", pt);
+            pt = strtok(NULL, ",");
+            printf("Velocity Direction: %s\n", pt);
+            pt = strtok(NULL, ",");
+            printf("Acceleration: %s\n", pt);
+            pt = strtok(NULL, ",");
+            printf("Brake Control: %s%%\n", pt);
+            pt = strtok(NULL, ",");
+            printf("Gas Throttle: %s%%\n\n", pt);
+        
+        // Respond to server:
+        strcpy(client_message, okMessage);
+        
+        if (sendto(socket_desc, client_message, strlen(client_message), 0,
+            (struct sockaddr*)&server_addr, server_struct_length) < 0){
+            printf("Can't send\n");
+            return -1;
+        }
     }
-   
-    //parse server_message
-    printf("Sequence Number: "); 
-	char *pt;
-	pt = strtok(server_message,",");
-		printf("%s\n", pt);
-		pt = strtok(NULL, ",");
-		printf("Source Address: %s\n", inet_ntoa(server_addr.sin_addr));
-		printf("GPS Position: %s, ", pt);
-		pt = strtok(NULL, ",");
-		printf("%s\n", pt);
-		pt = strtok(NULL, ",");
-		printf("Velocity Magnitude: %s\n", pt);
-		pt = strtok(NULL, ",");
-		printf("Velocity Direction: %s\n", pt);
-		pt = strtok(NULL, ",");
-		printf("Acceleration: %s\n", pt);
-		pt = strtok(NULL, ",");
-		printf("Brake Control: %s%%\n", pt);
-		pt = strtok(NULL, ",");
-		printf("Gas Throttle: %s%%\n", pt);
-    
-    // Respond to server:
-    strcpy(client_message, okMessage);
-    
-    if (sendto(socket_desc, client_message, strlen(client_message), 0,
-         (struct sockaddr*)&server_addr, server_struct_length) < 0){
-        printf("Can't send\n");
-        return -1;
-    }
-    
     // Close the socket:
     close(socket_desc);
-
 }
--- udp_server.c
+++ udp_server.c
@@ -19,145 +19,149 @@ int main(void){
     char server_message[100], client_message[100];
     int client_struct_length = sizeof(client_addr);
     
-    // Clean buffers:
-    memset(server_message, '\0', sizeof(server_message));
-    memset(client_message, '\0', sizeof(client_message));
-    
     // Create socket:
     socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
     if(socket_desc < 0){
-        printf("Error while creating socket\n");
+        printf("Error while creating socket\n\n");
         return -1;
     }
-    printf("Socket created successfully\n");
+    printf("Socket created successfully\n\n");
     
     // Set port and IP:
     client_addr.sin_family = AF_INET;
     client_addr.sin_port = htons(2000);
     client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     
+    //Random set
+    srand(time(NULL));
+
     // Get input from the user:
     //printf("Enter message: ");
     //fgets(server_message, 100, stdin);
     
-    //Generate Message --------------------------------------------------------
+    //Loop the main logic 100 times to recieve 100 packets
+    for(int i = 0; i < 100; i++){
 
-    //Variables
-    int loopNumber = 1;
-    srand(time(NULL));
+        // Clean buffers:
+        memset(server_message, '\0', sizeof(server_message));
+        memset(client_message, '\0', sizeof(client_message));
 
-    //Calculate Sequence Number
-    char sequenceNumber[2];
-    snprintf(sequenceNumber, sizeof(sequenceNumber), "%d", loopNumber);
+        //Generate Message ----------------------------------------------------
 
-    //Calculate GPS Position
-    char gpsPositionPartOne[8];
-    char gpsPositionPartTwo[8];
-    float gpsPositionPartOneRaw = ((float)rand()/(float)(RAND_MAX))*10*10;
-    float gpsPositionPartTwoRaw = ((float)rand()/(float)(RAND_MAX))*10*10;
-    snprintf(gpsPositionPartOne, sizeof(gpsPositionPartOne), "%0.5f", gpsPositionPartOneRaw);
-    snprintf(gpsPositionPartTwo, sizeof(gpsPositionPartTwo), "%0.5f", gpsPositionPartTwoRaw);
+        //Calculate Sequence Number
+        char sequenceNumber[2];
+        snprintf(sequenceNumber, sizeof(sequenceNumber), "%d", i+1);
 
-    //Calculate Velocity Direction
-    int velocityDirRandIndex = (rand() %(7 - 0 + 1)) + 0;
-    char velocityDir[3];
-    if (velocityDirRandIndex == 0){
-        strcpy(velocityDir, "N");
-    }
-    else if (velocityDirRandIndex == 1){
-       strcpy(velocityDir, "NE");
-    }
-    else if (velocityDirRandIndex == 2){
-        strcpy(velocityDir, "E");
-    }
-    else if (velocityDirRandIndex == 3){
-        strcpy(velocityDir, "SE");
-    }
-    else if (velocityDirRandIndex == 4){
-        strcpy(velocityDir, "S");
-    }
-    else if (velocityDirRandIndex == 5){
-        strcpy(velocityDir, "SW");
-    }
-    else if (velocityDirRandIndex == 6){
-        strcpy(velocityDir, "W");
-    }
-    else{
-        strcpy(velocityDir, "NW");
-    }
+        //Calculate GPS Position
+        char gpsPositionPartOne[8];
+        char gpsPositionPartTwo[8];
+        float gpsPositionPartOneRaw = ((float)rand()/(float)(RAND_MAX))*10*10;
+        float gpsPositionPartTwoRaw = ((float)rand()/(float)(RAND_MAX))*10*10;
+        snprintf(gpsPositionPartOne, sizeof(gpsPositionPartOne), "%0.5f", gpsPositionPartOneRaw);
+        snprintf(gpsPositionPartTwo, sizeof(gpsPositionPartTwo), "%0.5f", gpsPositionPartTwoRaw);
 
-    //Calculate Velocity Magnitude
-    char velocityMag[6];
-    float velocityMagRaw = ((float)rand()/(float)(RAND_MAX))*10*10;
-    snprintf(velocityMag, sizeof(velocityMag), "%0.1f", velocityMagRaw);
+        //Calculate Velocity Direction
+        int velocityDirRandIndex = (rand() %(7 - 0 + 1)) + 0;
+        char velocityDir[3];
+        if (velocityDirRandIndex == 0){
+            strcpy(velocityDir, "N");
+        }
+        else if (velocityDirRandIndex == 1){
+        strcpy(velocityDir, "NE");
+        }
+        else if (velocityDirRandIndex == 2){
+            strcpy(velocityDir, "E");
+        }
+        else if (velocityDirRandIndex == 3){
+            strcpy(velocityDir, "SE");
+        }
+        else if (velocityDirRandIndex == 4){
+            strcpy(velocityDir, "S");
+        }
+        else if (velocityDirRandIndex == 5){
+            strcpy(velocityDir, "SW");
+        }
+        else if (velocityDirRandIndex == 6){
+            strcpy(velocityDir, "W");
+        }
+        else{
+            strcpy(velocityDir, "NW");
+        }
 
-    //Calculate Acceleration Magnitude
-    char acceleration[5];
-    float accelerationRaw = ((float)rand()/(float)(RAND_MAX))*10;
-    int isNegative = (rand() %(9 - 0 + 1)) + 0;
-    if (isNegative == 1){
-        accelerationRaw = accelerationRaw*(-1);
-    }
-    snprintf(acceleration, sizeof(acceleration), "%0.1f", accelerationRaw);
+        //Calculate Velocity Magnitude
+        char velocityMag[6];
+        float velocityMagRaw = ((float)rand()/(float)(RAND_MAX))*10*10;
+        snprintf(velocityMag, sizeof(velocityMag), "%0.1f", velocityMagRaw);
 
-    //Calculate brake value
-    int brakeControlRaw;
-    char brakeControl[3];
-    if (accelerationRaw < 0){
-        brakeControlRaw =  (rand() %(100 - 10 + 1)) + 10;
-    }
-    else{
-        brakeControlRaw =  0;
-    }
-    snprintf(brakeControl, sizeof(brakeControl), "%d", brakeControlRaw);
+        //Calculate Acceleration Magnitude
+        char acceleration[5];
+        float accelerationRaw = ((float)rand()/(float)(RAND_MAX))*10;
+        int isNegative = (rand() %(9 - 0 + 1)) + 0;
+        if (isNegative == 1){
+            accelerationRaw = accelerationRaw*(-1);
+        }
+        snprintf(acceleration, sizeof(acceleration), "%0.1f", accelerationRaw);
 
-    //Calculate throttle value
-    int gasThrottleRaw;
-    char gasThrottle[3];
-    if (accelerationRaw > 0){
-        gasThrottleRaw =  (rand() %(100 - 10 + 1)) + 10;
-    }
-    else{
-        gasThrottleRaw =  0;
-    }
-    snprintf(gasThrottle, sizeof(gasThrottle), "%d", gasThrottleRaw);
+        //Calculate brake value
+        int brakeControlRaw;
+        char brakeControl[3];
+        if (accelerationRaw < 0){
+            brakeControlRaw =  (rand() %(100 - 10 + 1)) + 10;
+        }
+        else{
+            brakeControlRaw =  0;
+        }
+        snprintf(brakeControl, sizeof(brakeControl), "%d", brakeControlRaw);
 
-    //Combine all calculated values into message
-    strcat(server_message, sequenceNumber);
-    strcat(server_message, ",");
-    strcat(server_message, gpsPositionPartOne);
-    strcat(server_message, ",");
-    strcat(server_message, gpsPositionPartTwo);
-    strcat(server_message, ",");
-    strcat(server_message, velocityDir);
-    strcat(server_message, ",");
-    strcat(server_message, velocityMag);
-    strcat(server_message, ",");
-    strcat(server_message, acceleration);
-    strcat(server_message, ",");
-    strcat(server_message, brakeControl);
-    strcat(server_message, ",");
-    strcat(server_message, gasThrottle);
-    //puts(server_message);
-//-----------------------------------------------------------------------------
+        //Calculate throttle value
+        int gasThrottleRaw;
+        char gasThrottle[3];
+        if (accelerationRaw > 0){
+            gasThrottleRaw =  (rand() %(100 - 10 + 1)) + 10;
+        }
+        else{
+            gasThrottleRaw =  0;
+        }
+        snprintf(gasThrottle, sizeof(gasThrottle), "%d", gasThrottleRaw);
 
-    // Send the message to client:
-    if(sendto(socket_desc, server_message, strlen(server_message), 0,
-         (struct sockaddr*)&client_addr, client_struct_length) < 0){
-        printf("Unable to send message\n");
-        return -1;
+        //Combine all calculated values into message
+        strcat(server_message, sequenceNumber);
+        strcat(server_message, ",");
+        strcat(server_message, gpsPositionPartOne);
+        strcat(server_message, ",");
+        strcat(server_message, gpsPositionPartTwo);
+        strcat(server_message, ",");
+        strcat(server_message, velocityDir);
+        strcat(server_message, ",");
+        strcat(server_message, velocityMag);
+        strcat(server_message, ",");
+        strcat(server_message, acceleration);
+        strcat(server_message, ",");
+        strcat(server_message, brakeControl);
+        strcat(server_message, ",");
+        strcat(server_message, gasThrottle);
+        //puts(server_message);
+
+        //Message generated ---------------------------------------------------
+
+        // Send the message to client:
+        if(sendto(socket_desc, server_message, strlen(server_message), 0,
+            (struct sockaddr*)&client_addr, client_struct_length) < 0){
+            printf("Unable to send message\n");
+            return -1;
+        }
+        
+        // Receive the client's response:
+        if(recvfrom(socket_desc, client_message, sizeof(client_message), 0,
+            (struct sockaddr*)&client_addr, &client_struct_length) < 0){
+            printf("Error while receiving server's msg\n");
+            return -1;
+        }
+        
+        printf("Packet %d Response: %s\n", i+1, client_message);
+        sleep(1);
     }
-    
-    // Receive the client's response:
-    if(recvfrom(socket_desc, client_message, sizeof(client_message), 0,
-         (struct sockaddr*)&client_addr, &client_struct_length) < 0){
-        printf("Error while receiving server's msg\n");
-        return -1;
-    }
-    
-    printf("Clients's response: %s\n", client_message);
-    
     // Close the socket:
     close(socket_desc);
     
